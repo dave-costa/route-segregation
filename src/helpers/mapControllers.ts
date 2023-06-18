8cbdb7d06ee02c0e519474554beb9148c60a5db6
Dave Costa <contact@prodave.tech>
2023-06-18 23:47:09 +0100

    refactor: mapControllers

--- src/helpers/mapControllers.ts
+++ src/helpers/mapControllers.ts
@@ -1,15 +1,4 @@
 export { mapControllers }
-// make const called person with very datas about person called dave, things how name, age ... ultil fifty things
-const person = {
-  name: "dave",
-  age: 50,
-  address: {
-    street: "street",
-    city: "city",
-    state: "state",
-    country: "country",
-  },
-}
 
 const mapControllers = (controller: any, dataControllers: any) => {
   for (const dataController of dataControllers) {
